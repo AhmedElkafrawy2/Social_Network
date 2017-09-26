bab7d32d6c79f692ae5a3377de6bbbb41a0d7b55
Ahmed Elkafrawy <ahmedelkafrawy088@gmail.com>
2017-09-26 14:31:23 +0200

    Finishing login and register pages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Auth;
 
 class HomeController extends Controller
 {
@@ -15,9 +14,7 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-
     }
-    
 
     /**
      * Show the application dashboard.
@@ -28,6 +25,4 @@ class HomeController extends Controller
     {
         return view('home');
     }
-    
-
 }
